defa3adbdb84d5ba8b7414b08ae3a3feda47e71a
milosdjurica <milosdjurica13@gmail.com>
2024-07-01 19:10:52 +0200

    more tests

--- test/Store.t.sol
+++ test/Store.t.sol
@@ -17,11 +17,47 @@ contract StoreUnitTests is Test {
     function test_storeNote_Success() public {
         string memory testNote = "Hello, Foundry!";
         store.storeNote(testNote);
-        string memory realNote = store.getNote(1);
-        assertEq(realNote, testNote, "Real note should match the test note");
+        string memory returnedNote = store.getNote(1);
+        assertEq(returnedNote, testNote, "Real note should match the test note");
     }
 
     function test_storeNote_BadID() public view {
-        assertEq(store.getNote(999), "", "Bad ID should return empty string");
+        string memory returnedNote = store.getNote(999);
+        assertEq(returnedNote, "", "Bad ID should return empty string");
+    }
+
+    function test_storeNote_StoreMultipleNotes() public {
+        string memory note1 = "First note";
+        string memory note2 = "Second note";
+
+        store.storeNote(note1);
+        store.storeNote(note2);
+
+        string memory returnedNote1 = store.getNote(1);
+        string memory returnedNote2 = store.getNote(2);
+
+        assertEq(returnedNote1, note1, "First stored note should match note1");
+        assertEq(returnedNote2, note2, "Second stored note should match note2");
+    }
+
+    function testStoreNoteDifferentUsers() public {
+        string memory noteUser1 = "User1's note";
+        string memory noteUser2 = "User2's note";
+
+        address user1 = makeAddr("user1");
+        address user2 = makeAddr("user2");
+
+        vm.prank(user1);
+        store.storeNote(noteUser1);
+
+        vm.prank(user2);
+        store.storeNote(noteUser2);
+
+        string memory returnedNoteUser1 = store.getNote(1);
+        assertEq(returnedNoteUser1, noteUser1, "User1's stored note should match");
+
+        vm.prank(user2);
+        string memory returnedNoteUser2 = store.getNote(2);
+        assertEq(returnedNoteUser2, noteUser2, "User2's stored note should match");
     }
 }
